e9605f231891e099f6d36383fde48a3035fd5fa0
Bonggt <kepy1106@gmail.com>
2019-03-31 17:35:20 +0900

    chpter 9

--- passport/index.js
+++ passport/index.js
@@ -8,34 +8,28 @@ module.exports = passport => {
     done(null, user.id);
   });
 
-  // req.session 객체로 부터 받은 id를 이용해 사용자 정보를 조회하여 req.user 객체에 저장
+  // // req.session 객체로 부터 받은 id를 이용해 사용자 정보를 조회하여 req.user 객체에 저장
   passport.deserializeUser(async (id, done) => {
     try {
-      const user = await User.findOne({ where: { id } });
-      return done(null, user);
+      let user = await User.findOne({
+        where: { id },
+        include: [
+          {
+            model: User,
+            attributes: ["id", "nick"],
+            as: "Followers"
+          },
+          {
+            model: User,
+            attributes: ["id", "nick"],
+            as: "Followings"
+          }
+        ]
+      });
+      user = await done(null, user);
     } catch (error) {
-      return done(error);
+      console.error(error);
     }
-  });
-
-  passport.deserializeUser((id, done) => {
-    User.findOne({
-      where: { id },
-      include: [
-        {
-          model: User,
-          attributes: ["id", "nick"],
-          as: "Followers"
-        },
-        {
-          model: User,
-          attributes: ["id", "nick"],
-          as: "Followings"
-        }
-      ]
-    })
-      .then(user => done(null, user))
-      .catch(error => done(error));
   });
 
   local(passport);
